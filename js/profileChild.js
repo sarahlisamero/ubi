b7ee35d49e3886b1329a68c0cd7e29abe2ddbbac
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-06-06 17:25:51 +0200

    feat: open and close dashboard link

---
+++ js/profileChild.js
@@ -0,0 +1,17 @@
+document.querySelector(".boost").addEventListener("click", giveBoost);
+
+function giveBoost(){
+    document.querySelector("#boost").classList.remove("hidden");
+}
+//nog met ajax en php ervoor zorgen dat aantal punten ook verminderd
+
+let clicks = 1;
+document.querySelector("header img").addEventListener("click", openDash);
+function openDash(){
+    clicks++;
+    if(clicks%2){
+        document.querySelector("#switch").classList.add("hidden");
+    } else{
+        document.querySelector("#switch").classList.remove("hidden");
+    }
+}
