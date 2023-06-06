9ee30e2c75becc938acc3728f14811b768e64854
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-06-06 18:01:35 +0200

    feat: battery

--- js/profileChild.js
+++ js/profileChild.js
@@ -2,6 +2,7 @@ document.querySelector(".boost").addEventListener("click", giveBoost);
 
 function giveBoost(){
     document.querySelector("#boost").classList.remove("hidden");
+    document.querySelector(".battery").setAttribute("src","img/highbat.svg");
 }
 //nog met ajax en php ervoor zorgen dat aantal punten ook verminderd
 
