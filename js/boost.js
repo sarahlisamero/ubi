2fe47538ce38743e377679148cab93c8d503dd61
Sarah-Lisa Mero <116017801+sarahlisamero@users.noreply.github.com>
2023-06-07 16:20:43 +0200

    Delete boost.js

--- js/boost.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".boost").addEventListener("click", giveBoost);
-
-function giveBoost(){
-    document.querySelector("#boost").classList.remove("hidden");
-}
-//nog met ajax en php ervoor zorgen dat aantal punten ook verminderd
