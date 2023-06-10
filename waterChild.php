7c6d0df42b8893abaf199131dd8461003c871b16
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-06-10 17:16:49 +0200

    chore: layout water feature

--- waterChild.php
+++ waterChild.php
@@ -22,8 +22,54 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.typekit.net/nnr1bhn.css">
+    <link rel="stylesheet" href="https://use.typekit.net/nnr1bhn.css">
     <title>Water drinken</title>
     <style>
+        header{
+            display: flex;
+            margin-left: 20px;
+            margin-top: 20px;
+        }
+        header img{
+            width:40px;
+            height:40px;
+        }
+        header div{
+            background-color: #FAE5B8;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 50px;
+            padding-left: 20px;
+            padding-right: 20px;
+            margin-right: 20px;
+        }
+        .intro{
+            padding-top:20px;
+            display:flex;
+            flex-direction:row;
+            justify-content: center;
+            align-items: center;
+            margin-bottom: 50px;
+            padding-left:40px;
+            padding-right: 40px;
+        }
+        .intro img{
+            width: 300px;
+        }
+        h1{
+            font-family: "futura-pt-bold", sans-serif;
+            font-weight: 700;
+            font-style: normal;
+            letter-spacing:1px;
+        }
+        p{
+            color: #050505;
+            font-family: "sofia-pro", sans-serif;
+            font-size: 18px;
+            letter-spacing:1px;
+        }
         .container {
         position: relative;
         display: flex;
@@ -31,9 +77,9 @@
         background-color: #F6F6F6;
         flex-wrap: wrap;
         max-width: 50%;
-        margin: 80px auto;
+        margin: 10px auto;
         padding-top: 50px;
-        padding-bottom: 50px;
+        padding-bottom: 100px;
         padding-left: 50px;
         padding-right: 50px;
         border-radius: 10px;
@@ -68,19 +114,38 @@
         transition: width 0.3s ease; 
         border-radius:30px;
         }
+        #day{
+            display: flex;
+            justify-content: center;
+        }
     </style>
 </head>
 <body>
-  <div class="container">
-    <div class="glass" id="glass1"></div>
-    <div class="glass" id="glass2"></div>
-    <div class="glass" id="glass3"></div>
-    <div class="glass" id="glass4"></div>
-    <div class="glass" id="glass5"></div>
-    <div class="glass" id="glass6"></div>
-    <div class="loading-bar"></div>
-  </div>
-
-  <script src="js/water.js"></script>
+    <header>
+        <div>
+            <img src="img/drinkbw.png" alt="drink icon">
+        </div>
+        <h1>Water drinken</h1>
+    </header>
+    <div class="intro">
+    <img class="avatar" src="<?php echo $childInfo['avatar']; ?>" alt="avatar">
+        <div>
+            <p>Houd hier bij hoeveel glaasjes je dronk. Duid elke keer aan wat je dronk door op het glas te klikken.</p>
+        </div>
+    </div>
+    <div id="day">
+        <p>Vandaag</p>
+    </div>
+    <div class="container">
+        <div class="glass" id="glass1"></div>
+        <div class="glass" id="glass2"></div>
+        <div class="glass" id="glass3"></div>
+        <div class="glass" id="glass4"></div>
+        <div class="glass" id="glass5"></div>
+        <div class="glass" id="glass6"></div>
+        <div class="loading-bar"></div>
+    </div>
+    <?php include_once("navchild.php")?>
+    <script src="js/water.js"></script>
 </body>
 </html>
